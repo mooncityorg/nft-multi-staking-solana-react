cde46142fdacfb068ce0988b4325e97ac75ee656
devdegen1 <shinydv412@gmail.com>
2022-04-14 04:16:11 +0900

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
-export const PROGRAM_ID = "Zq5BGzpnYDgcYTB7v5mgaSg4buQbXvEu9JE8gFe9NPJ";
+export const PROGRAM_ID = "";
 
 export const PUBLISH_NETWIRK = "devnet"; // "devnet" or "mainnet"
 
-export const ADMIN_KEY = "Ek464DsENbDmMWTxSBNRn1S3xuxkixwiS5ceWoRLSjFw";
+export const ADMIN_KEY = "";
 
-export const INIT_WALLETKEY = "Ek464DsENbDmMWTxSBNRn1S3xuxkixwiS5ceWoRLSjFw";
+export const INIT_WALLETKEY = "";
 
 export const LOCK_TIME = 1000 * 60 * 10 // 10 is days
 
@@ -12,9 +12,9 @@ export const REWARD_EPOCH = 1000 * 60 * 2 // Daily
 
 export const REWARD_UNIT = 0.02 // SOL
 
-export const LAND_CREATOR = "tQuU2g2JGUA8dyPEXgxuVF3d3Nbiv49HiKWZJQKzimR";
-export const FARMER_CREATOR = "9nwyX2j8o52TQChq7whMRVH3p5DDyv7wWefjrXeYASnC";
-export const ANIMAL_CREATOR = "G42V1DfQKKHrxxfdjDrRphPStZx5Jqu2JwShfN3WoKmK";
+export const LAND_CREATOR = "";
+export const FARMER_CREATOR = "";
+export const ANIMAL_CREATOR = "";
 
 export const LAND_MAX = 1;
 export const FARMER_MAX = 3;
